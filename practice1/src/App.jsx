7516a4b872044d770bb4b39a42804cb232977c05
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-02 23:20:04 +0530

    1. MyComponent

---
+++ practice1/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ practice1/src/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+function MyComponent() {
+    
+    
+    return(
+        <div className="container">
+            <div className="clock">
+                <span> 00 </span>
+            </div>
+        </div>
+    );
+}
+export default MyComponent
+
